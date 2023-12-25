f6aad7775d53e9c2f6c85f737d10dcf139491ce5
Aleksey.Bukin <eltorwin@yandex.ru>
2023-12-25 08:12:17 +0500

    add validation to model exchange_rate

--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class ExchangeRate < ApplicationRecord
+    validates :current_rate, presence: true, uniqueness: { 
+        scope: [:current_rate, :parse_at]
+    }
+
+    validates :current_value, presence: true
+    validates :parse_at, presence: true
+
     enum current_rate: {
         rub_usd: 'rub_usd',
         rub_eur: 'rub_eur',
